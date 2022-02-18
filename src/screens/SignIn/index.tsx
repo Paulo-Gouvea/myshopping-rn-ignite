3bb254f2549a08323703cf195b21e32194999f53
Paulo-Gouvea <paulogouvea1994@gmail.com>
2022-02-18 14:43:46 -0300

    feat: finished the app

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,11 +1,66 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+import auth from '@react-native-firebase/auth';
 
 import { Container, Account, Title, Subtitle } from './styles';
 import { ButtonText } from '../../components/ButtonText';
 import { Button } from '../../components/Button';
 import { Input } from '../../components/Input';
+import { Alert } from 'react-native';
 
 export function SignIn() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  async function handleSignInAnonymously(){
+    const { user } = await auth().signInAnonymously();
+    console.log(user);
+  }
+
+  function handleCreateUserAccount(){
+    auth()
+      .createUserWithEmailAndPassword(email, password)
+      .then(() => {
+        Alert.alert('Usuário criado com sucesso!');
+      })
+      .catch(error => {
+        console.log(error.code);
+
+        if(error.code === 'auth/email-already-in-use'){
+          return Alert.alert("Este e-mail já está em uso.")
+        }
+
+        if(error.code === 'auth/invalid-email'){
+          return Alert.alert("E-mail inválido!")
+        }
+
+        if(error.code === 'auth/weak-password'){
+          return Alert.alert("A senha deve ter no minimo 6 dígitos!")
+        }
+      })
+  }
+  
+  async function handleSignInWithEmailAndPassword(){
+    auth()
+      .signInWithEmailAndPassword(email, password)
+      .then(({ user })=> console.log(user))
+      .catch(error => {
+        console.log(error)
+
+        if(error.code === 'auth/user-not-found' || error.code === 'auth/wrong-password'){
+          Alert.alert('Usuário não encontrado', 'E-mail ou senha inválida!');
+        }
+      });
+  }
+
+  function handleForgotPassword(){
+    auth()
+    .sendPasswordResetEmail(email)
+    .then(()=>{
+      Alert.alert("Enviamos um link no seu e-mail para você redefinir sua senha.");
+    })
+  }
+
   return (
     <Container>
       <Title>MyShopping</Title>
@@ -14,18 +69,20 @@ export function SignIn() {
       <Input
         placeholder="e-mail"
         keyboardType="email-address"
+        onChangeText={setEmail}
       />
 
       <Input
         placeholder="senha"
         secureTextEntry
+        onChangeText={setPassword}
       />
 
-      <Button title="Entrar" onPress={() => { }} />
+      <Button title="Entrar" onPress={handleSignInWithEmailAndPassword} />
 
       <Account>
-        <ButtonText title="Recuperar senha" onPress={() => { }} />
-        <ButtonText title="Criar minha conta" onPress={() => { }} />
+        <ButtonText title="Recuperar senha" onPress={handleForgotPassword} />
+        <ButtonText title="Criar minha conta" onPress={handleCreateUserAccount} />
       </Account>
     </Container>
   );
